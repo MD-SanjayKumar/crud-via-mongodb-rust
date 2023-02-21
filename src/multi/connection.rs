547c9501f2fa4ad7d93b9c64a0ed8553b09bd081
sanjaykumar <sanjay.kumar@minddeft.net>
2023-02-21 13:43:19 +0530

    commited

--- src/multi/connection.rs
+++ src/multi/connection.rs
@@ -7,7 +7,7 @@ use mongodb::{
     Client, Collection,
 };
 use mongoose::Document;
-use std::{io};
+use std::io;
 
 pub async fn conn_str() {
     let client_option = ClientOptions::parse_with_resolver_config(
@@ -17,8 +17,9 @@ pub async fn conn_str() {
     .await
     .expect("Not found");
     let client = Client::with_options(client_option).expect("Not found");
-    let data: mongodb::Collection<Document> =
-        client.database("crud_operation_rust").collection("user_data");
+    let data: mongodb::Collection<Document> = client
+        .database("crud_operation_rust")
+        .collection("user_data");
 
     let mut usrnm = String::new();
     let mut usradd = String::new();
@@ -32,32 +33,35 @@ pub async fn conn_str() {
 
     let now = chrono::offset::Local::now();
     let res = now.format("%Y-%m-%d %H:%M:%S");
-    if usrname.to_string().trim() == "" || usrnm.to_string().trim() == "" || usradd.to_string().trim() == ""{
+    if usrname.to_string().trim() == ""
+        || usrnm.to_string().trim() == ""
+        || usradd.to_string().trim() == ""
+    {
         println!("Please enter valid input.");
-    }else{
-    //insert
-    let detail = Details {
-        id: None,
-        username: usrname,
-        name: usrnm,
-        address: usradd,
-        //datetime: Utc::now(),
-        datetime: res.to_string()
-    };
+    } else {
+        //insert
+        let detail = Details {
+            id: None,
+            username: usrname,
+            name: usrnm,
+            address: usradd,
+            //datetime: Utc::now(),
+            datetime: res.to_string(),
+        };
 
-    let sDetails = bson::to_bson(&detail).expect("Found error");
-    let docs = sDetails.as_document().unwrap();
+        let sDetails = bson::to_bson(&detail).expect("Found error");
+        let docs = sDetails.as_document().unwrap();
 
-    let result = data
-        .insert_one(docs.to_owned(), None)
-        .await
-        .expect("Found error");
-    let sid = result
-        .inserted_id
-        .as_object_id()
-        .expect("Retrieved _id should have been of type ObjectId");
-    println!("Record Inserted\n(Document ID: {:?})", sid);
-}
+        let result = data
+            .insert_one(docs.to_owned(), None)
+            .await
+            .expect("Found error");
+        let sid = result
+            .inserted_id
+            .as_object_id()
+            .expect("Retrieved _id should have been of type ObjectId");
+        println!("Record Inserted\n(Document ID: {:?})", sid);
+    }
 }
 
 pub async fn fetch_data() {
@@ -68,31 +72,31 @@ pub async fn fetch_data() {
     .await
     .expect("Not found");
     let client = Client::with_options(client_option).expect("Not found");
-    let data: mongodb::Collection<Document> =
-        client.database("crud_operation_rust").collection("user_data");
+    let data: mongodb::Collection<Document> = client
+        .database("crud_operation_rust")
+        .collection("user_data");
     let mut usrname = String::new();
     println!("Enter username :");
     io::stdin().read_line(&mut usrname).expect("Not found");
-    if usrname.to_string().trim() == ""{
+    if usrname.to_string().trim() == "" {
         println!("Please enter valid username.");
-    }else{
-    //     //fetch
-    let fetch: Document = data
-        .find_one(
-            doc! {
-                  "username": usrname.to_string()
-            },
-            None,
-        )
-        .await
-        .expect("error found")
-        .expect("Missing 'Parasite' document.");
-    println!(
-        "Details -\nName :{:?}\nAddress :{:?}\nDate :{:?}",
-        fetch.get_str("name").unwrap().trim(),
-        fetch.get_str("address").unwrap().trim(),
-        fetch.get_str("datetime").unwrap().trim()
-        //fetch.get_datetime("datetime").unwrap().to_string()
-    );
-}
+    } else {
+        //     //fetch
+        let fetch: Document = data
+            .find_one(
+                doc! {
+                      "username": usrname.to_string()
+                },
+                None,
+            )
+            .await
+            .expect("error found")
+            .expect("Missing 'Parasite' document.");
+        println!(
+            "Details -\nName :{:?}\nAddress :{:?}\nDate :{:?}",
+            fetch.get_str("name").unwrap().trim(),
+            fetch.get_str("address").unwrap().trim(),
+            fetch.get_str("datetime").unwrap().trim() //fetch.get_datetime("datetime").unwrap().to_string()
+        );
+    }
 }
--- src/multi/update.rs
+++ src/multi/update.rs
@@ -16,84 +16,89 @@ pub async fn update_str() {
     .await
     .expect("Not found");
     let client = Client::with_options(client_option).expect("Not found");
-    let data: mongodb::Collection<Document> =
-        client.database("crud_operation_rust").collection("user_data");
+    let data: mongodb::Collection<Document> = client
+        .database("crud_operation_rust")
+        .collection("user_data");
     let mut usrname = String::new();
     let mut usrnm = String::new();
     let mut usradd = String::new();
     println!("Enter username :");
     io::stdin().read_line(&mut usrname).expect("Not found");
-    if usrname.to_string().trim() == ""{
+    if usrname.to_string().trim() == "" {
         println!("Please enter valid username.");
-    }else{
-    let fetch: Document = data
-        .find_one(
-            doc! {
-                  "username": usrname.to_string()
-            },
-            None,
-        )
-        .await
-        .expect("error found")
-        .expect("Missing 'Parasite' document.");
-    let mut case_match = true;
-    while case_match {
-        let mut v = String::new();
-        println!("- - - - - -\n1.Update name\n2.Update address\n- - - - - -\nSelect one option:");
-        io::stdin().read_line(&mut v).expect("Not found");
-        match v.as_str().trim() {
-            "1" => {
-                println!("Enter new name :");
-                io::stdin().read_line(&mut usrnm).expect("Not found");
-                if usrnm.to_string().trim() == ""{
-                    println!("Please enter valid name.");
-                }else{
-                let update_result = data
-                    .update_one(
-                        doc! {
-                           "_id": &fetch.get("_id")
-                        },
-                        doc! {
-                           "$set": { "name": usrnm.to_string() }
-                        },
-                        None,
-                    )
-                    .await
-                    .expect("Error found");
-                println!(
-                    "Name updated successfully.\nUpdated {} document",
-                    update_result.modified_count
-                );
-                case_match = false;
-            }},
-            "2" => {
-                println!("Enter new address :");
-                io::stdin().read_line(&mut usradd).expect("Not found");
-                if usradd.to_string().trim() == ""{
-                    println!("Please enter valid address.");
-                }else{
-                let update_result = data
-                    .update_one(
-                        doc! {
-                           "_id": &fetch.get("_id")
-                        },
-                        doc! {
-                           "$set": { "address": usradd.to_string() }
-                        },
-                        None,
-                    )
-                    .await
-                    .expect("Error found");
-                println!(
-                    "Address updated successfully.\nUpdated {} document",
-                    update_result.modified_count
-                );
-                case_match = false;
-            }}
-            _ => println!("Please select one option."),
+    } else {
+        let fetch: Document = data
+            .find_one(
+                doc! {
+                      "username": usrname.to_string()
+                },
+                None,
+            )
+            .await
+            .expect("error found")
+            .expect("Missing 'Parasite' document.");
+        let mut case_match = true;
+        while case_match {
+            let mut v = String::new();
+            println!(
+                "- - - - - -\n1.Update name\n2.Update address\n- - - - - -\nSelect one option:"
+            );
+            io::stdin().read_line(&mut v).expect("Not found");
+            match v.as_str().trim() {
+                "1" => {
+                    println!("Enter new name :");
+                    io::stdin().read_line(&mut usrnm).expect("Not found");
+                    if usrnm.to_string().trim() == "" {
+                        println!("Please enter valid name.");
+                    } else {
+                        let update_result = data
+                            .update_one(
+                                doc! {
+                                   "_id": &fetch.get("_id")
+                                },
+                                doc! {
+                                   "$set": { "name": usrnm.to_string() }
+                                },
+                                None,
+                            )
+                            .await
+                            .expect("Error found");
+                        println!(
+                            "Name updated successfully.\nUpdated {} document",
+                            update_result.modified_count
+                        );
+                        case_match = false;
+                    }
+                }
+                "2" => {
+                    println!("Enter new address :");
+                    io::stdin().read_line(&mut usradd).expect("Not found");
+                    if usradd.to_string().trim() == "" {
+                        println!("Please enter valid address.");
+                    } else {
+                        let update_result = data
+                            .update_one(
+                                doc! {
+                                   "_id": &fetch.get("_id")
+                                },
+                                doc! {
+                                   "$set": { "address": usradd.to_string() }
+                                },
+                                None,
+                            )
+                            .await
+                            .expect("Error found");
+                        println!(
+                            "Address updated successfully.\nUpdated {} document",
+                            update_result.modified_count
+                        );
+                        case_match = false;
+                    }
+                }
+                _ => println!("Please select one option."),
+            }
         }
     }
-}
 }
 
 pub async fn delete_str() {
@@ -104,27 +109,28 @@ pub async fn delete_str() {
     .await
     .expect("Not found");
     let client = Client::with_options(client_option).expect("Not found");
-    let data: mongodb::Collection<Document> =
-        client.database("crud_operation_rust").collection("user_data");
+    let data: mongodb::Collection<Document> = client
+        .database("crud_operation_rust")
+        .collection("user_data");
     let mut usrname = String::new();
     println!("Enter username :");
     io::stdin().read_line(&mut usrname).expect("Not found");
-    if usrname.to_string().trim() == ""{
+    if usrname.to_string().trim() == "" {
         println!("Please enter valid username.");
-    }else{
-    let delete_result = data
-        .delete_many(
-            doc! {
-               "username": usrname.to_string()
-            },
-            None,
-        )
-        .await
-        .expect("Error found");
-    println!(
-        "> {}Deleted Successfully.\nDeleted {} documents",
-        usrname.to_string(),
-        delete_result.deleted_count
-    );
-}
+    } else {
+        let delete_result = data
+            .delete_many(
+                doc! {
+                   "username": usrname.to_string()
+                },
+                None,
+            )
+            .await
+            .expect("Error found");
+        println!(
+            "> {}Deleted Successfully.\nDeleted {} documents",
+            usrname.to_string(),
+            delete_result.deleted_count
+        );
+    }
 }
